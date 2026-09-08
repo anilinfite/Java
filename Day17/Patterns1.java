744903e240b3fdb8367fc1c4cc04efc4ab7d96a0
anilinfite <biradaranilkumar36@gmail.com>
2026-09-08 17:02:47 +0530

    updates the Patterns

---
+++ Day17/Patterns1.java
@@ -0,0 +1,22 @@
+package Day17;
+
+public class Patterns1 {
+    public static void main(String[] args)
+    {
+     int count = 0;
+     for(int i=1; i<=5; i++)
+        {
+            
+        for(int j=1; j<=5; j++)
+            {
+            ++count;
+            System.out.print( count+" ");
+             ++count;
+        }
+        System.out.println();
+    }  
+           
+        
+   
+    }
+}
